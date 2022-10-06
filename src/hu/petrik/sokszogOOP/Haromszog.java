202f93889460f95784e51898dfc2b60cf2c3c5fe
Krisztián <radeczki.krisztian@gmail.com>
2022-10-06 17:03:54 +0200

    óra fele

---
+++ src/hu/petrik/sokszogOOP/Haromszog.java
@@ -0,0 +1,92 @@
+package hu.petrik.sokszogOOP;
+
+public class Haromszog extends Sokszog{
+
+    private double b;
+    private double c;
+
+    public Haromszog(double a, double b, double c) {
+        super(a);
+        this.b = b;
+        this.c = c;
+        if (!this.isSzerkesztehto()){
+            throw new IllegalArgumentException("A háromszög nem szerkeszthető");
+        }
+    }
+
+    public Haromszog(){
+        super(veletlenOldalGeneralas());
+        this.b = veletlenOldalGeneralas();
+        this.c = veletlenOldalGeneralas();
+        while (!this.isSzerkesztehto()){
+            super.setA(veletlenOldalGeneralas());
+            this.b = veletlenOldalGeneralas();
+            this.c = veletlenOldalGeneralas();
+        }
+
+    }
+
+    private static double veletlenOldalGeneralas() {
+        return Math.random() * 10 + 5;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+        if (!this.isSzerkesztehto()){
+            throw new IllegalArgumentException("A háromszög nem szerkeszthető");
+        }
+
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+        if (!this.isSzerkesztehto()){
+            throw new IllegalArgumentException("A háromszög nem szerkeszthető");
+        }
+    }
+
+    @Override
+    public void setA(double a) {
+        super.setA(a);
+        if (!this.isSzerkesztehto()){
+            throw new IllegalArgumentException("A háromszög nem szerkeszthető");
+        }
+
+    }
+
+
+    public double getKerulet(){
+        return this.getA() + this.getB() + this.getC();
+    }
+
+    private double getS(){
+        return getKerulet() / 2;
+    }
+
+    private boolean isSzerkesztehto(){
+        return this.getA() + this.getB() > this.getC() &&
+                this.getA() + this.getC() > this.getB() &&
+                this.getB() + this.getC() > this.getA();
+    }
+
+
+    public double getTerulet(){
+        return Math.sqrt(this.getS() * (this.getS() - this.getA())
+                *(this.getS() - this.getB())
+                *(this.getS() - this.getC()));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Háromszög: a = %-10.3f b = %-10.3f C = %-10.3f K = %-10.3f T = %-10.3f",
+                this.getA(),this.getB(),this.getC(),this.getKerulet(),this.getTerulet());
+    }
+}
--- src/hu/petrik/sokszogOOP/Main.java
+++ src/hu/petrik/sokszogOOP/Main.java
@@ -1,5 +1,20 @@
 package hu.petrik.sokszogOOP;
 
 public class Main {
+    public static void main(String[] args){
+        for (int i =0; i < 3; i++){
+            double a = Math.random() * 5 + 10;
+            double b = Math.random() * 5 + 10;
+            Teglalap t = new Teglalap(a,b);
+            System.out.println(t);
+        }
 
+        Haromszog h1 = new Haromszog(3,4,5);
+        System.out.println(h1);
+        System.out.println();
+        for (int i = 0; i < 100; i++) {
+            Haromszog h = new Haromszog();
+            System.out.println(h);
+        }
+    }
 }
